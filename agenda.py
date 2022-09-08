992060a5bde565df9d7e8ac00749d7143dec75f8
Lucas Gustavo Oldiges <111318820+LucasOldiges@users.noreply.github.com>
2022-09-07 23:31:08 -0300

    primeiro commit

---
+++ agenda.py
@@ -0,0 +1,82 @@
+from optparse import OptionContainer
+
+
+def menu():
+    voltarMenuPrincipal = 's'
+    while voltarMenuPrincipal=='s':
+        opcao= input('''==================================
+                                AGENDA TELEFÔNICA
+    MENU:
+
+    [1]CADASTRAR CONTATO
+    [2]LISTAR CONTATO
+    [3]DELETAR CONTATO
+    [4]BUSCAR CONTATO PELO NOME
+    [5]SAIR
+    ======================================================
+    ESCOLHA UMA OPÇÃO ACIMA:
+    ''')
+        if opcao =="1":
+            cadastrarContato()
+        elif opcao== "2":
+            listarContato ()
+        elif opcao == "3":
+            deletarContato()
+        elif opcao=="4":
+            buscarContatoPeloNome() 
+        else:
+            sair()
+        voltarMenuPrincipal=input("Deseja voltar ao menu principal ? (s/n) ").lower()
+
+def cadastrarContato():
+    idContato = input("Escolha o Id do contato: ")
+    nome = input("Escreva o nome do contato: ")
+    telefone = input("Escreva o telefone do contato: ")
+    email = input("Escreva o email do contato: ")
+    try:
+       agenda = open("agenda.txt","a")
+       dados = f'{idContato};{nome};{telefone};{email}\n'
+       agenda.write(dados)
+       agenda.close()
+       print(f'Contato gravado com sucesso!')
+    except:
+        print("Erro na gravação do contato")
+
+def listarContato():
+    agenda = open("agenda.txt","r")
+    for contato in agenda:
+        print(contato)
+    agenda.close()
+
+def deletarContato():
+    nomeDeletado = input("Digite o nome que deseja excluir: ").lower()
+    agenda = open("agenda.txt","r")
+    aux = []
+    aux2= []
+    for i in agenda:
+        aux.append(i)
+    for i in range(0, len(aux)):
+        if nomeDeletado not in aux[i].lower():
+             aux2.append(aux[i])
+    agenda = open("agenda.txt","w")
+    for i in aux2:
+        agenda.write(i)
+    print(f'Contato deletado com sucesso') 
+    listarContato()           
+
+def buscarContatoPeloNome():
+    nome = input(f'Digite o nome a ser procurado: ').upper()
+    agenda = open("agenda.txt","r")
+    for contato in agenda:
+        if nome in contato.split(";")[1].upper():
+         print(contato)
+    agenda.close() 
+
+def sair():
+    print(f'Até logo...')
+    exit()
+
+
+def main():
+    menu()
+main()        
